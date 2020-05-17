0b920bf0455da3d5c8da10c2b85ed5d11dc3631c
黄东彪 <302803997@qq.com>
2020-05-17 11:58:11 +0800

    完成用户列表功能开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,140 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container style="height: 100vh">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt style="width: 60px" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主题区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单 -->
+        <el-menu
+          unique-opened
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="$route.path"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menulist: [],
+      iconsObj: {
+        "125": "el-icon-user-solid",
+        "103": "el-icon-s-check",
+        "101": "el-icon-s-goods",
+        "102": "el-icon-s-order",
+        "145": "el-icon-s-data"
+      },
+      isCollapse: false
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
   methods: {
     logout() {
       window.sessionStorage.removeItem("token");
       this.$router.push("/login");
+    },
+    async getMenuList() {
+      // 获取菜单
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menulist = res.data;
+    },
+    // 折叠展开按钮效果
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.el-header {
+  background-color: #373d37;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: 0;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.el-submenu span {
+  margin-left: 10px;
+}
+
+.toggle-button {
+  background-color: #4a5046;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import '../css/global.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem("token")
+  // 在最后必须return config
+  return config
+
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
